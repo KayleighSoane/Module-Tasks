9b7bbc078984a1499bcb7dcdda40e23e3ce2acb1
Kayleigh Soane <kayleigh.soane@students.plymouth.ac.uk>
2025-11-03 17:40:11 +0000

    Finished version

--- 1001Wk6-C/1001Wk6-C/Introduction to C.cpp
+++ 1001Wk6-C/1001Wk6-C/Introduction to C.cpp
@@ -2,10 +2,17 @@
 #include <windows.h> //this library is needed for pause() function
 
 int array1[5], array2[5], sum[5];
-
 void initialize();
 void add();
 void print();
+
+int A[10];
+void initialize2();
+void print2();
+int findmax();
+
+int findmin();
+
 
 void initialize() {
 	int i;
@@ -15,14 +22,12 @@ void initialize() {
 		sum[i] = 0;
 	}
 }
-
 void add() {
 	int i;
 	for (i = 0; i < 5; i++) {
 		sum[i] = array1[i] + array2[i];
 	}
 }
-
 void print() {
 	int i;
 	for (i = 0; i < 5; i++) {
@@ -38,6 +43,40 @@ void print() {
 	}
 }
 
+
+void initialize2() {
+	int i;
+	for (i = 0; i < 10; i++) {
+		A[i] = rand() % 20;
+	}
+}
+int findmax() {
+	int i, max;
+	max = A[0];
+	for (i = 1; i < 10; i++) {
+		if (A[i] > max)
+			max = A[i];
+	}
+	return max;
+}
+void print2() {
+	int i;
+	for (i = 0; i < 10; i++) {
+		printf("%d ", A[i]);
+	}
+}
+
+int findmin() {
+	int i, min;
+	min = A[0];
+	for (i = 1; i < 10; i++) {
+		if (A[i] < min)
+			min = A[i];
+	}
+	return min;
+}
+
+
 void main() {
 	printf("Size of char: %lu\n", sizeof(char));
 	printf("Size of short: %lu\n", sizeof(short));
@@ -48,6 +87,7 @@ void main() {
 	printf("Size of double: %lu\n", sizeof(double));
 	printf("Size of long double: %lu\n", sizeof(long double));
 	system("pause"); //this function prevents from closing the output window
+	printf("\n");
 
 
 	initialize();
@@ -55,26 +95,58 @@ void main() {
 	print();
 	printf("\n");
 	system("pause");
+	printf("\n");
 
 
 	char name[20];
-	printf("\nEnter your name: "); //get input from keyboard
+	printf("Enter your name: "); //get input from keyboard
 	scanf_s("%19s", name, sizeof(name));
-	printf("\nYour name is %s\n", name);
+	printf("Your name is %s\n", name);
 	system("pause"); //this command does not let the output window to close
+	printf("\n");
 
 
 	char string[1000];//input string
 	char pattern[2]; //pattern string. The first character is the one we need to find in the string array. the second character is always zero as it is the last character of the array
 	unsigned int counter = 0;
-	printf("\nHi - Enter a large string: ");
+	printf("Hi - Enter a large string: ");
 	scanf_s("%999s", string, sizeof(string)); //read the string from keyboard
 	printf("Enter a character to find its frequency: ");
-	scanf_s("%1s", pattern, sizeof(pattern));
+	scanf_s("%s", pattern, sizeof(pattern));
 	for (int i = 0; string[i] != '\0'; ++i) {
 		if (pattern[0] == string[i])
 			counter++;
 	}
-	printf("Frequency of character %c is %d\n\n", pattern[0], counter);
+	printf("Frequency of character %c is %d\n", pattern[0], counter);
+	system("pause");
+	printf("\n");
 
+
+	char stringlen[1000];//input string
+	printf("Hi - Enter a large string: ");
+	scanf_s("%s", stringlen, sizeof(stringlen)); //read the string from keyboard
+	printf("Length of String: %ld\n", strlen(stringlen));
+	system("pause");
+	printf("\n");
+
+
+	int var1 = 4;
+	printf("The memory address of %p contains %d\n", &var1, var1);
+	system("pause");
+	printf("\n");
+
+
+	int max;
+	initialize2();
+	print2();
+	max = findmax();
+	printf("Max value in the array is: %d\n", max);
+	system("pause");
+	printf("\n");
+
+	int min;
+	min = findmin();
+	printf("Min value in the array is: %d\n", min);
+	system("pause");
+	printf("\n");
 } 
