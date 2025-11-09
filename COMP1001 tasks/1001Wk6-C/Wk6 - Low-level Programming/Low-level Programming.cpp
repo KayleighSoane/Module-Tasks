3b2b5cef1791fde15226b626c7f141363101f33b
Kayleigh Soane <kayleigh.soane@students.plymouth.ac.uk>
2025-11-09 20:46:45 +0000

    Finished wk6

--- COMP1001 tasks/1001Wk6-C/Wk6 - Low-level Programming/Low-level Programming.cpp
+++ COMP1001 tasks/1001Wk6-C/Wk6 - Low-level Programming/Low-level Programming.cpp
@@ -15,12 +15,29 @@ int main()
 	int i;
 	struct student arr_student[N];
 
-	printf("Student Record System\n");
-	printf("---------------------\n\n");
-	printf("Student name: ");
-	scanf_s("%s", arr_student[i].student_name);
-	printf("Module code: ");
-	scanf_s("%d", &arr_student[i].module_code);
-	printf("Mark: ");
-	scanf_s("%f", &arr_student[i].mark);
+	for (i = 0; i < N; i++) {
+		printf("Student Record System\n");
+		printf("---------------------\n\n");
+		printf("Student %d name: ", i + 1);
+		scanf_s("%s", arr_student[i].student_name, sizeof(arr_student[i].student_name));
+		printf("Module code: ");
+		scanf_s("%d", &arr_student[i].module_code, sizeof(arr_student[i].module_code));
+		printf("Mark: ");
+		scanf_s("%f", &arr_student[i].mark, sizeof(arr_student[i].mark));
+	}
+
+	int n = 0;
+	for (i = 0; i < N; i++) {
+		printf("\nEnter Student 1 or 2: ");
+		scanf_s("%d", &n, sizeof(n));
+		if (n == 1) {
+			printf("%s\t%d\t%.2f\n", arr_student[0].student_name, arr_student[0].module_code, arr_student[0].mark);
+		}
+		else if (n == 2) {
+			printf("%s\t%d\t%.2f\n", arr_student[1].student_name, arr_student[1].module_code, arr_student[1].mark);
+		}
+
+		system("pause");
+		return 0;
+	}
 }
